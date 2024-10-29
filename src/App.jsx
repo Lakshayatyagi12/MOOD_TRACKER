14662c7c6a02d79b30922cfff42cbc1399b74758
Lakshaya Tyagi <92645154+Lakshayatyagi12@users.noreply.github.com>
2024-10-30 05:11:24 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ const App = () => {
       <ErrorBoundary>
     <div className='min-h-screen bg-gradient-to-br from-purple-400 to-indigo-600 
     dark:form-gray-600 dark:to-gray-900 transition-colors duration-200 shadow-2xl'> 
-    <div className="max-w-4xl mx-auto ps-4 sm:px-6 lg:px-8 py-6 sm:py-8 lg:py-12">
+    <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 py-6 sm:py-8 lg:py-12">
       <div className='bg-white dark:bg-gray-800 rounded-xl shadow-xl overflow-hidden transition-colors duration-200 '>
         <Toaster />
       <Header activeTab={activeTab} setActiveTab={setActiveTab}/>
